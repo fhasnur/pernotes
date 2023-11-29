98c52907f354a0dcf40b40eda13c4ead8f8ddd93
fhasnur <fandi.hasnur10@gmail.com>
2023-11-29 23:01:42 +0800

    feat: add notes modal and refactor card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,39 @@
-import CardHeader from "./CardHeader"
-import CardBody from "./CardBody"
-import CardButton from "./CardButton"
+import Button from "./Button"
+import { FaRegNoteSticky } from "react-icons/fa6"
+import { RiInboxArchiveLine } from "react-icons/ri"
+import { FiEdit, FiTrash } from "react-icons/fi"
 
 const Card = () => {
   return (
     <div className="card p-8 bg-custom-primary rounded-2xl text-custom-txt-primary hover:shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] lg:mb-2 transition-all transform duration-300 ease-in-out">
-      <CardHeader />
-      <CardBody />
-      <CardButton />
+      <div>
+        <h2 className="font-medium text-2xl">Babel</h2>
+        <h5 className="font-light text-sm ">28 November 2023</h5>
+      </div>
+      <div>
+        <p className="my-6 line-clamp-6">Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.  JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX. JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.</p>
+      </div>
+      <div className="flex items-center justify-between mt-1 mb-1">
+        <Button variant="primary" className="w-1/2 font-normal lg:-ml-1 hover:bg-[#D9DDDA]">
+          <FaRegNoteSticky size={15} />
+          Read more
+        </Button>
+        <div className="tooltip tooltip-bottom" data-tip="archive">
+          <Button variant="primary" className="hover:bg-[#D9DDDA]">
+            <RiInboxArchiveLine size={16} />
+          </Button>
+        </div>
+        <div className="tooltip tooltip-bottom" data-tip="edit">
+          <Button variant="primary" className="hover:bg-[#D9DDDA]">
+            <FiEdit size={16} />
+          </Button>
+        </div>
+        <div className="tooltip tooltip-bottom" data-tip="delete">
+          <Button variant="primary" className="hover:bg-[#D9DDDA]">
+            <FiTrash size={16} />
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/ContentHeader.jsx
+++ src/components/ContentHeader.jsx
@@ -1,29 +1,28 @@
-import Button from "./Button"
-import { FiPlusCircle } from "react-icons/fi"
-import { MdOutlineArchive } from "react-icons/md"
+// import { useState } from 'react'
+import Button from './Button'
+import { FiPlusCircle } from 'react-icons/fi'
+import { MdOutlineArchive } from 'react-icons/md'
+import Modal from './Modal'
 
 const ContentHeader = () => {
-  const handleClick = () => {
-    console.log('Button clicked!');
-  };
-
   return (
     <div className="flex items-center my-8">
       <div className="flex-1">
         <h1 className="text-2xl text-custom-txt font-medium lg:-ml-2">📝 Notes</h1>
       </div>
       <div className="flex-none">
-        <Button onClick={handleClick} variant="secondary" className="ml-2 px-5 py-3 hover:bg-[#3261F2]">
+        <Button onClick={() => document.querySelector('button.hidden').click()} variant="secondary" className="ml-2 px-5 py-3 hover:bg-[#3261F2]">
           <FiPlusCircle size={17} />
           Add notes
         </Button>
-        <Button onClick={handleClick} variant="tertiary" className="ml-2 px-5 py-3  hover:bg-[#242B41]">
+        <Button onClick={() => console.log('Archive button clicked')} variant="tertiary" className="ml-2 px-5 py-3 hover:bg-[#242B41]">
           <MdOutlineArchive size={18} />
           Archive
         </Button>
       </div>
+      <Modal />
     </div>
-  )
-}
+  );
+};
 
 export default ContentHeader
